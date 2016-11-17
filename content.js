526e6f18d3c0a3b731bae113cca4c0c1f1ed9888
Danila Parkhomenko <thereaintnomailformeagain@gmail.com>
2016-11-17 04:55:05 +0300

    Supporting back card doubling (kind of... print is been called before body changes are actually taking place)

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
-//alert("Hello from your Chrome extension!")
+var addedStyle = "border: 1px solid #000000;";
+var ellipsis = '<br><i>см. след. карт.</i>';
+var secondCardPrefix = '<p align="center">- 2 -</p>';
+
 
 String.prototype.truncate = function(n, ellipsis){
     var reg = new RegExp("[\\w]*[\\S]+", "g");
@@ -21,24 +24,27 @@ String.prototype.truncate = function(n, ellipsis){
    return this;
 }
 
+
     function addCardIfNeeded() {
-        var biblioDiv = document.getElementById('cc-biblio');//$("cc-biblio")
+        var biblioDiv = document.getElementById('cc-biblio');
         //var positionInfo = biblioDiv.getBoundingClientRect();
         //console.log(positionInfo)
+        if (biblioDiv == null) return;
         if (biblioDiv.clientHeight < biblioDiv.scrollHeight) {
             var text = biblioDiv.innerHTML;//.replace(/&nbsp;/gi, " ");
-            var splitPoint = text.length / 2;
-            var ellipsis = ' <i>(Продолжение на следующей карточке)</i>';
+            var splitPoint = text.length * 12 / 20;
             var firstCardText = text.truncate(splitPoint, ellipsis);
             splitPoint = firstCardText.length - ellipsis.length;
-            var secondCardText = text.substr(splitPoint, text.length - splitPoint);
+            var secondCardText = secondCardPrefix + text.substr(splitPoint, text.length - splitPoint);
             
             biblioDiv.innerHTML = firstCardText;
+            var parentDiv = document.getElementsByClassName("catcard")[0];
+            parentDiv.style.cssText += addedStyle;
             var divCopyTop = document.getElementById('cc-top-left').innerHTML;
             var divCopyBottomLeft = document.getElementById('cc-bottom-left').innerHTML;
             var divCopyBottomRight = document.getElementById('cc-bottom-right').innerHTML;
-            var addedCardText = '<br>'
-            addedCardText += '<div class="catcard cc-bs">';
+            var addedCardText = '<br><br>'
+            addedCardText += '<div class="catcard cc-bs" style="' + addedStyle + '">';
             addedCardText += '    <div id="cc-top" class="cc-bs">';
             addedCardText += '        <div id="cc-top-left" class="cc-bs">'
             addedCardText += divCopyTop;
@@ -58,11 +64,34 @@ String.prototype.truncate = function(n, ellipsis){
             addedCardText += '</div>';
             document.body.innerHTML += addedCardText;
         }
-        //console.log(biblioDiv.clientHeight);
-        //console.log(biblioDiv.scrollHeight);
-        //console.log(biblioDiv.innerHTML);
     }
 
-//$(document).ready(addCardIfNeeded);
-
+    function addBackCardIfNeeded() {
+        var biblioDiv = document.getElementById('backcard');
+        console.log(biblioDiv);
+        if (biblioDiv == null) return false;
+        if (biblioDiv.clientHeight < biblioDiv.scrollHeight) {
+            var text = biblioDiv.innerHTML;//.replace(/&nbsp;/gi, " ");
+            var splitPoint = text.length * 12 / 20;
+            var firstCardText = text.truncate(splitPoint, ellipsis);
+            splitPoint = firstCardText.length - ellipsis.length;
+            var secondCardText = secondCardPrefix + text.substr(splitPoint, text.length - splitPoint);
+            
+            biblioDiv.innerHTML = firstCardText;
+            biblioDiv.style.cssText += addedStyle;
+            var addedCardText = '<br><br>'
+            addedCardText += '<div class="catcard" style="' + addedStyle + '">';
+            addedCardText += secondCardText;
+            addedCardText += '</div>';
+            document.body.innerHTML += addedCardText;
+            return true;
+        }
+        return false;
+    }
+    
 addCardIfNeeded();
+
+addBackCardIfNeeded();
+
+//setTimeout(function () { print();}, 5000);
+
